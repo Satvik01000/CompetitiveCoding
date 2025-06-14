3a7459b77010f965013310fc890226132c083929
Satvik Mishra <satvikmishra2192@gmail.com>
2025-06-14 15:32:40 +0530

    Solved the Make it beautiful problem

--- Contests/Make It Beautiful/Make It Beautiful.cpp
+++ Contests/Make It Beautiful/Make It Beautiful.cpp
@@ -1,46 +1,49 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define ll long long
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
 
-bool cmp(pair<ll, ll> a, pair<ll, ll> b){
-    return a.first>b.first;
-}
-
-ll maxPossible(vector<ll>& nums, ll n, ll k){
-    vector<pair<ll, ll>> setBits(n);
-    for(ll i=0; i<nums.size(); i++)
-        setBits[i]={__builtin_popcount(nums[i]), nums[i]};
-    sort(setBits.begin(), setBits.end(), cmp);
-
-    ll sum=0;
-    if(k!=0){
-        for(ll i=0; i<n && k>0; i++){
-            ll temp = __builtin_popcount(setBits[i].second+1);
-            if(temp > setBits[i].first){
-                setBits[i].first=temp;
-                k--;
-            }
-            sum+=setBits[i].first;
+int maxBeautyPossible(vector<int64_t>& a, int n, int64_t k) {
+    vector<int> all;
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < 60; j++) {
+            if ((a[i] >> j) & 1)
+                ans += 1;
+            else
+                all.push_back(j);
         }
-    }else{
-        for(auto& i : setBits)
-            sum+=i.first;
     }
-    return sum;
+    sort(all.begin(), all.end());
+    for (int x : all) {
+        int64_t need = 1LL << x;
+        if (k >= need) {
+            k -= need;
+            ans += 1;
+        } else {
+            break;
+        }
+    }
+    return ans;
 }
 
-int main(){
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     int t;
-    cin>>t;
+    cin >> t;
+    while (t--) {
+        int n;
+        int64_t k;
+        cin >> n >> k;
+        vector<int64_t> a(n);
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
 
-    while(t--){
-        ll n, k;
-        cin>>n>>k;
-
-        vector<ll> nums(n);
-        for(ll& i : nums)
-            cin>>i;
-        cout<<maxPossible(nums, n, k)<<endl;
+        cout << maxBeautyPossible(a, n, k) << '\n';
     }
     return 0;
 }
