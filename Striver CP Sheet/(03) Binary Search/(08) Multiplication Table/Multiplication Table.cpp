71894318b57c03f0abf76d6ca4750923fdc9ca4d
Satvik Mishra <satvikmishra2192@gmail.com>
2025-06-19 17:14:42 +0530

    Added solution for Multiplication Table

--- Striver CP Sheet/(03) Binary Search/(08) Multiplication Table/Multiplication Table.cpp
+++ Striver CP Sheet/(03) Binary Search/(08) Multiplication Table/Multiplication Table.cpp
@@ -3,13 +3,18 @@
 using namespace std;
  
 bool check(ll x, ll n, ll m, ll k) {
-    ll cnt = 0;
-    for (ll i = 1; i <= n; i++)
-        cnt += min(x / i, m);
-    return cnt>=k;
+    ll count = 0;
+    for (ll i = 1; i <= n; i++){
+        if(x/i==0)
+            break;
+        count += min(x / i, m);
+    }
+    return count>=k;
 }
  
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     ll n, m, k;
     cin >> n >> m >> k;
  
