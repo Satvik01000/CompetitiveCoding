94260778ad70bd82a0c8e9ea5741127108f4c821
Satvik Mishra <satvikmishra2192@gmail.com>
2026-01-13 18:49:47 +0530

    Solved Hourglass

--- Contests/Hourglass.cpp
+++ Contests/Hourglass.cpp
@@ -3,8 +3,16 @@ using namespace std;
 #define fast ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 #define ll long long
 
-int sandFallAfterLeave(int s, int k, int m){
+ll sandFallAfterLeave(ll s, ll k, ll m){
+    if(s <= k) {
+        m %= k;
+        if(m < s) return s - m;
+        return 0;
+    }
     
+    m %= (2 * k);
+    if(m < k) return s - m;
+    return k - (m - k);
 }
 
 int main() {
@@ -12,9 +20,9 @@ int main() {
     int t;
     cin >> t;
     while (t--) {
-        int s, k, m;
+        ll s, k, m;
         cin>>s>>k>>m;
-        cout<<sandFallAfterLeave(s, k, n)<<endl;
+        cout<<sandFallAfterLeave(s, k, m)<<endl;
     }
     return 0;
 }
