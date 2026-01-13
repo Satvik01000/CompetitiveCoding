a5abbbd36798b2c9cef2b89a681563bf53bb67e2
Satvik Mishra <satvikmishra2192@gmail.com>
2026-01-13 17:57:48 +0530

    Upsolved Social Exper

---
+++ Contests/Hourglass.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define ll long long
+
+int sandFallAfterLeave(int s, int k, int m){
+    
+}
+
+int main() {
+    fast;
+    int t;
+    cin >> t;
+    while (t--) {
+        int s, k, m;
+        cin>>s>>k>>m;
+        cout<<sandFallAfterLeave(s, k, n)<<endl;
+    }
+    return 0;
+}
